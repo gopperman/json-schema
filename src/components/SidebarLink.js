8840b7666c71e8ecad02f07371fc5445da1980f6
Greg Opperman <gopperman@gmail.com>
2017-09-21 08:54:09 -0400

    Adding hrefs for field sections

--- src/components/SidebarLink.js
+++ src/components/SidebarLink.js
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function SidebarLink(props) {
-	const {active, name} = props
+	const {active, id, name} = props
 	const linkClass = `sidebar__link ${active ? 'sidebar__link--active' : ''}`
 
 	return (
-			<a className={linkClass}>{name}</a>
+			<a href={`#field-item__${id}`} className={linkClass}>{name}</a>
 	)
 }
 
